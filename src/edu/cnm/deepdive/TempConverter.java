a39a18ee9f0dd3ac16ca75b339a0036fb2be02a7
darbach <danielarbach@gmail.com>
2021-02-08 15:25:15 -0700

    Some Javadocs

--- src/edu/cnm/deepdive/TempConverter.java
+++ src/edu/cnm/deepdive/TempConverter.java
@@ -1,5 +1,9 @@
 package edu.cnm.deepdive;
 
+/**
+ * Simple class to demonstrate reading properties and iterating over
+ * arguments.
+ */
 public class TempConverter {
 	
 	public static void main(String[] args) {
@@ -18,11 +22,23 @@ public class TempConverter {
 		}
 	}
 	
+	/**
+	 * Convert temperature fahrenheit to celsius.
+	 *
+	 * @param tempF Temperature in Fahrenheit degrees.
+	 * @return Temperature in Celsius degrees.
+	 */
 	public static float convertFToC(float tempF) {
 		float tempC = (tempF - 32) * 5 / 9;
 		return tempC;
 	}
 	
+	/**
+	 * Convert temperature celsius to fahrenheit.
+	 *
+	 * @param tempC Temperature in Celsius degrees.
+	 * @return Temperature in Fahrenheit degrees.
+	 */
 	public static float convertCToF(float tempC) {
 		float tempF = tempC * 9 / 5 + 32;
 		return tempF;
@@ -31,7 +47,7 @@ public class TempConverter {
 
 /*
 change directory to the TempConverter project folder
-javac -d out src\edu\cnm\deepdive\TempConverter     <--- look for sourcecode to compile to bytecode .class file
+javac -d out src\edu\cnm\deepdive\*.java     <--- look for sourcecode to compile to bytecode .class file
 														 directory structure must match the package path
 java -cp out edu.cnm.deepdive.TempConverter 212     <--- full qualified class name inside the "out" directory
 
@@ -41,7 +57,9 @@ git add .      <---add everything in it to the staging area
 git commit -m "Simple functionality implemented"  <---50 characters or less
 git remote add origin git@github.com:darbach/temp-converter.git  <---link it to a repository created on github
 git status      <--- get the origin
-git push --set-upstream origin master   <--- initial push the repository to git
+git push --set-upstream origin master   <---set the default upstream
 java -cp out -Dmode=c2f edu.cnm.deepdive.TempConverter -273.15 0 -40 100
 java -cp out -Dmode=f2c edu.cnm.deepdive.TempConverter 212 32 -40  OR remove the -Dmode argument
+javadoc -d docs\api -sourcepath src -subpackages edu -link https://docs.oracle.com/en/java/javase/11/docs/api/   <---generate javadoc html for all sourcecode in edu subdirectories, link option will insert links to other library documentation
+jar cfe TempConverter.jar edu.cnm.deepdive.TempConverter -C out edu\cnm\deepdive\*.class  <--- create jar file from entry point everything inside -C directory
 */
