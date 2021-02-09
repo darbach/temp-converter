7a474e1cf10d42fe116373d85605edfbf60f988b
darbach <danielarbach@gmail.com>
2021-02-09 08:38:46 -0700

    Cleaned up notes

--- src/edu/cnm/deepdive/TempConverter.java
+++ src/edu/cnm/deepdive/TempConverter.java
@@ -47,19 +47,17 @@ public class TempConverter {
 
 /*
 change directory to the TempConverter project folder
-javac -d out src\edu\cnm\deepdive\*.java     <--- look for sourcecode to compile to bytecode .class file
-														 directory structure must match the package path
+javac -d out src\edu\cnm\deepdive\*.java     <--- look for sourcecode to compile to bytecode .class file directory structure must match the package path
 java -cp out edu.cnm.deepdive.TempConverter 212     <--- full qualified class name inside the "out" directory
-
 .gitignore     <---file with line "out" will not upload anything in this folder substructure
 git init       <---to create a repository
 git add .      <---add everything in it to the staging area
 git commit -m "Simple functionality implemented"  <---50 characters or less
 git remote add origin git@github.com:darbach/temp-converter.git  <---link it to a repository created on github
 git status      <--- get the origin
-git push --set-upstream origin master   <---set the default upstream
+git push --set-upstream origin master   <---set the default upstream origin
 java -cp out -Dmode=c2f edu.cnm.deepdive.TempConverter -273.15 0 -40 100
-java -cp out -Dmode=f2c edu.cnm.deepdive.TempConverter 212 32 -40  OR remove the -Dmode argument
+java -cp out -Dmode=f2c edu.cnm.deepdive.TempConverter 212 32 -40   OR remove the -Dmode argument
 javadoc -d docs\api -sourcepath src -subpackages edu -link https://docs.oracle.com/en/java/javase/11/docs/api/   <---generate javadoc html for all sourcecode in edu subdirectories, link option will insert links to other library documentation
 jar cfe TempConverter.jar edu.cnm.deepdive.TempConverter -C out edu\cnm\deepdive\TempConverter.class  <--- create jar file from entry point everything inside -C directory
 java -jar TempConverter.jar 212 -40 32
