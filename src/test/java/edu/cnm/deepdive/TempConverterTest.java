24eafcc6310c8dda9216703f80f39a9050383157
darbach <danielarbach@gmail.com>
2021-02-09 13:54:11 -0700

    Test cases in arrays.

--- src/test/java/edu/cnm/deepdive/TempConverterTest.java
+++ src/test/java/edu/cnm/deepdive/TempConverterTest.java
@@ -7,27 +7,33 @@ import org.junit.jupiter.api.Test;
 class TempConverterTest {
 
   //constant variables have "static final", SCREAMING_SNAKE_CASE
-  static final float CELSIUS_TOLERANCE = 0.001f;
+  static final float CELSIUS_TOLERANCE    = 0.001f;
   static final float FAHRENHEIT_TOLERANCE = 0.0018f;
-  static final float[] celsius = {-40,0,100};
-  static final float[] fahrenheit = {-40, 32, 212};
+  static final float[] celsius            = {-40, 0, 100};
+  static final float[] fahrenheit         = {-40, 32, 212};
 
   @Test
   void convertFToC() {
     assertEquals(100.55555, TempConverter.convertFToC(213), CELSIUS_TOLERANCE);
 
-    for (int i = 0; i < fahrenheit.length; i++) {
-
+    for (int i = 0; i < celsius.length; i++) {
+      float expected = celsius[i];
+      float actual = TempConverter.convertFToC(fahrenheit[i]);
+      System.out.printf("Expected celsius = %.3f; input fahrenheit = %.3f; actual celsius = %.3f%n",
+          expected, fahrenheit[i], actual);
+      assertEquals(expected, actual, CELSIUS_TOLERANCE);
     }
-    assertEquals(-40, TempConverter.convertFToC(-40), CELSIUS_TOLERANCE);
-    assertEquals(100, TempConverter.convertFToC(212), CELSIUS_TOLERANCE);
-    assertEquals(0, TempConverter.convertFToC(32), CELSIUS_TOLERANCE);
   }
 
   @Test
   void convertCToF() {
-    assertEquals(-40, TempConverter.convertCToF(-40), FAHRENHEIT_TOLERANCE);
-    assertEquals(212, TempConverter.convertCToF(100), FAHRENHEIT_TOLERANCE);
-    assertEquals(32, TempConverter.convertCToF(0), FAHRENHEIT_TOLERANCE);
+    for (int i = 0; i < fahrenheit.length; i++) {
+      float expected = fahrenheit[i];
+      float actual = TempConverter.convertCToF(celsius[i]);
+      System.out.printf(
+          "Expected fahrenheit = %.3f; input celsius = %.3f; actual fahrenheit = %.3f%n",
+          expected, celsius[i], actual);
+      assertEquals(fahrenheit[i], TempConverter.convertCToF(celsius[i]), FAHRENHEIT_TOLERANCE);
+    }
   }
 }
