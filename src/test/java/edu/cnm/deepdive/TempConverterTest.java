1a6cace90f0c87d2a0f6354e9f408aa99a9e954d
darbach <danielarbach@gmail.com>
2021-02-09 14:21:49 -0700

    Test cases in 2-d array.

--- src/test/java/edu/cnm/deepdive/TempConverterTest.java
+++ src/test/java/edu/cnm/deepdive/TempConverterTest.java
@@ -9,31 +9,45 @@ class TempConverterTest {
   //constant variables have "static final", SCREAMING_SNAKE_CASE
   static final float CELSIUS_TOLERANCE    = 0.001f;
   static final float FAHRENHEIT_TOLERANCE = 0.0018f;
-  static final float[] celsius            = {-40, 0, 100};
-  static final float[] fahrenheit         = {-40, 32, 212};
+
+  //
+  //   testCases (static final, this array reference canNOT be changed)
+  //       |
+  //       v
+  //      [0]--->[-40][-40]   \
+  //      [1]--->[0][32]       } not final, these references/data can be changed
+  //      [2]--->[100][212]   /
+  //
+  static final float[][] testCases = {
+      {-40, -40},
+      {0, 32},
+      {100, 212}
+  };
 
   @Test
   void convertFToC() {
     assertEquals(100.55555, TempConverter.convertFToC(213), CELSIUS_TOLERANCE);
 
-    for (int i = 0; i < celsius.length; i++) {
-      float expected = celsius[i];
-      float actual = TempConverter.convertFToC(fahrenheit[i]);
+    for (float[] testCase : testCases) {
+
+      float expected = testCase[0];
+      float input = testCase[1];
+      float actual = TempConverter.convertFToC(input);
       System.out.printf("Expected celsius = %.3f; input fahrenheit = %.3f; actual celsius = %.3f%n",
-          expected, fahrenheit[i], actual);
+          expected, input, actual);
       assertEquals(expected, actual, CELSIUS_TOLERANCE);
     }
   }
 
   @Test
   void convertCToF() {
-    for (int i = 0; i < fahrenheit.length; i++) {
-      float expected = fahrenheit[i];
-      float actual = TempConverter.convertCToF(celsius[i]);
-      System.out.printf(
-          "Expected fahrenheit = %.3f; input celsius = %.3f; actual fahrenheit = %.3f%n",
-          expected, celsius[i], actual);
-      assertEquals(fahrenheit[i], TempConverter.convertCToF(celsius[i]), FAHRENHEIT_TOLERANCE);
+    for (float[] testCase : testCases) {
+      float expected = testCase[1];
+      float input = testCase[0];
+      float actual = TempConverter.convertCToF(input);
+      System.out.printf("Expected fahrenheit = %.3f; input celsius = %.3f; actual fahrenheit = %.3f%n",
+          expected, input, actual);
+      assertEquals(expected, actual, FAHRENHEIT_TOLERANCE);
     }
   }
 }
